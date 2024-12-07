e031ccc1160ce95cbf8e7a293557ef2addc00c3d
G00410024@atu.ie <g00410024@atu.ie>
2024-12-07 22:10:43 +0000

    Delete function working

--- components/meetups/MeetupDetail.js
+++ components/meetups/MeetupDetail.js
@@ -1,12 +1,27 @@
+import { IoTrashBinOutline } from "react-icons/io5";
 import classes from './MeetupDetail.module.css'
+import GlobalContext from "../../pages/store/globalContext";
+import { useContext } from 'react'
+import { useRouter } from "next/router";
 
 function MeetupDetail(props) {
+    const globalCtx = useContext(GlobalContext)
+    const router = useRouter();
+
+    function deleteMeeting() {
+        globalCtx.updateGlobals({ cmd: 'deleteMeeting', meetingId: props.title })
+        router.push('/');
+    }
+
     return (
         <section className={classes.detail}>
             <img src={props.image} alt={props.title} />
             <h1>{props.title}</h1>
             <address>{props.address}</address>
             <p>{props.description}</p>
+            <IoTrashBinOutline 
+                className={classes.deleteBin} 
+                onClick={() => deleteMeeting()}/>
         </section>
     )
 }
