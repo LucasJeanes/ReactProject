ae914f11ebe3df90b08c921d224cce246038443b
alan-hynes <johndoe@example.com>
2024-12-10 10:52:57 +0000

    kjhgf

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -1,20 +1,39 @@
-import MeetupDetail from '../../components/meetups/MeetupDetail'
-import { useRouter } from 'next/router'
-import GlobalContext from "../../pages/store/globalContext"
-import { useContext } from 'react'
+import MeetupDetail from '../../components/meetups/MeetupDetail';
+import { useRouter } from 'next/router';
+import GlobalContext from "../../pages/store/globalContext";
+import { useContext } from 'react';
 
-export default function () {
-    const globalCtx = useContext(GlobalContext)
-    const router = useRouter();
+export default function MeetupDetailsPage() {
+  const globalCtx = useContext(GlobalContext);
+  const router = useRouter();
 
-    // Back to basics, a simple for loop. Also trim() comes into play as it usually does!
-    let returnVal = null
-    for (let ii = 0; ii < globalCtx.theGlobalObject.meetings.length; ii++) {
-        let temp = globalCtx.theGlobalObject.meetings[ii]
-        if (temp.meetingId.trim() == router.query.meetupId.trim()) {
-            returnVal = <MeetupDetail image={temp.image} title={temp.title} description={temp.description} />
-        }
+  console.log("Meetup ID from query:", router.query.meetupId);
+
+  if (!globalCtx.theGlobalObject || !globalCtx.theGlobalObject.meetings || !Array.isArray(globalCtx.theGlobalObject.meetings)) {
+    console.error("Meetings data is not available or invalid.");
+    return <p>Unable to load meetup details. Please try again later.</p>;
+  }
+
+  let returnVal = null;
+  for (let ii = 0; ii < globalCtx.theGlobalObject.meetings.length; ii++) {
+    let temp = globalCtx.theGlobalObject.meetings[ii];
+    if (temp && temp.meetingId && router.query.meetupId) {
+      if (temp.meetingId.trim() === router.query.meetupId.trim()) {
+        returnVal = (
+          <MeetupDetail
+            image={temp.image}
+            title={temp.title}
+            description={temp.description}
+          />
+        );
+        break;
+      }
     }
-    // In the real world, we'd put the code above in the store context module. 
-    return returnVal
+  }
+
+  if (!returnVal) {
+    return <p>Meetup not found. Please check the URL or try again.</p>;
+  }
+
+  return returnVal;
 }
